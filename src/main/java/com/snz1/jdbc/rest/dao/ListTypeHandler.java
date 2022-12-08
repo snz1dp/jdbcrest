9ea7133e6ad39cc77ef72e2aadf55c52b09fcea9
痞子飞猪 <13317312768@qq.com>
2022-12-08 11:06:52 +0800

    修改列表转换

--- src/main/java/com/snz1/jdbc/rest/dao/ListTypeHandler.java
+++ src/main/java/com/snz1/jdbc/rest/dao/ListTypeHandler.java
@@ -9,33 +9,70 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+
+import com.snz1.utils.JsonUtils;
 
 public class ListTypeHandler extends BaseTypeHandler<List<String>> {
 
   @Override
   public void setNonNullParameter(PreparedStatement ps, int i, List<String> parameter, JdbcType jdbcType)
       throws SQLException {
-    Connection conn = ps.getConnection();
-    String[] string_array = parameter.toArray(new String[0]);
-    Array array_value = conn.createArrayOf("varchar", string_array);
-    ps.setArray(i, array_value);
+    if (JdbcType.ARRAY.equals(jdbcType)) {
+      Connection conn = ps.getConnection();
+      String[] string_array = parameter.toArray(new String[0]);
+      Array array_value = conn.createArrayOf("varchar", string_array);
+      ps.setArray(i, array_value);
+    } else {
+      ps.setString(i, JsonUtils.toJson(parameter));
+    }
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public List<String> getNullableResult(ResultSet rs, String columnName) throws SQLException {
-    return getStringList(rs.getArray(columnName));
+    JdbcType jdbcType = JdbcType.ARRAY;
+    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+      if (StringUtils.equalsIgnoreCase(columnName, rs.getMetaData().getColumnName(i))) {
+        jdbcType = JdbcType.forCode(rs.getMetaData().getColumnType(i));
+        break;
+      }
+    }
+    if (JdbcType.ARRAY.equals(jdbcType)) {
+      return getStringList(rs.getArray(columnName));
+    } else {
+      String result_value = rs.getString(columnName);
+      if (result_value == null) return null;
+      return JsonUtils.fromJson(result_value, List.class);
+    }
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public List<String> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-    return getStringList(rs.getArray(columnIndex));
+    JdbcType jdbcType = JdbcType.forCode(rs.getMetaData().getColumnType(columnIndex));
+    if (JdbcType.ARRAY.equals(jdbcType)) {
+      return getStringList(rs.getArray(columnIndex));
+    } else {
+      String result_value = rs.getString(columnIndex);
+      if (result_value == null) return null;
+      return JsonUtils.fromJson(result_value, List.class);
+    }
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public List<String> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-    return getStringList(cs.getArray(columnIndex));
+    JdbcType jdbcType = JdbcType.forCode(cs.getMetaData().getColumnType(columnIndex));
+    if (JdbcType.ARRAY.equals(jdbcType)) {
+      return getStringList(cs.getArray(columnIndex));
+    } else {
+      String result_value = cs.getString(columnIndex);
+      if (result_value == null) return null;
+      return JsonUtils.fromJson(result_value, List.class);
+    }
   }
 
   private List<String> getStringList(Array array_value) throws SQLException {
