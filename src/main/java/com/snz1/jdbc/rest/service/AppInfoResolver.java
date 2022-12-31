b0f1ce98aa94644f2e71a0f6f196e05c1cf452df
痞子飞猪 <13317312768@qq.com>
2022-12-31 22:06:41 +0800

    添加授权信息

--- src/main/java/com/snz1/jdbc/rest/service/AppInfoResolver.java
+++ src/main/java/com/snz1/jdbc/rest/service/AppInfoResolver.java
@@ -2,6 +2,8 @@ package com.snz1.jdbc.rest.service;
 
 import com.snz1.jdbc.rest.Version;
 
+import gateway.sc.v2.config.LicenseSupport;
+
 // 应用信息获取
 public interface AppInfoResolver {
 
@@ -10,5 +12,11 @@ public interface AppInfoResolver {
 
   // 获取版本
   Version getVersion();
+
+  // 获取授权
+  LicenseSupport getLicense();
+
+  // 是否有授权
+  boolean hasLicense();
 
 }
--- src/main/java/com/snz1/jdbc/rest/service/impl/AppInfoResolverImpl.java
+++ src/main/java/com/snz1/jdbc/rest/service/impl/AppInfoResolverImpl.java
@@ -2,17 +2,33 @@ package com.snz1.jdbc.rest.service.impl;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.snz1.jdbc.rest.RunConfig;
 import com.snz1.jdbc.rest.Version;
 import com.snz1.jdbc.rest.service.AppInfoResolver;
+import com.snz1.utils.Configurer;
 
+import gateway.api.NotExceptException;
+import gateway.sc.v2.ToolProvider;
+import gateway.sc.v2.config.LicenseSupport;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class AppInfoResolverImpl implements AppInfoResolver {
 
   @Resource
   private RunConfig runConfig;
+
+  @Resource
+  private ToolProvider toolProvider;
+
+  private String lastLicense;
+
+  private LicenseSupport licenseSupport;
+
 
   @Override
   public String getAppId() {
@@ -22,6 +38,26 @@ public class AppInfoResolverImpl implements AppInfoResolver {
   @Override
   public Version getVersion() {
     return runConfig.getAppVerison();
+  }
+
+  public boolean hasLicense() {
+    return this.getLicense() != null;
+  }
+
+  @Override
+  public LicenseSupport getLicense() {
+    String lic = Configurer.getAppProperty("license.code", "<NOT SUPPORT>");
+    if (StringUtils.equals(lic, this.lastLicense)) {
+      return this.licenseSupport;
+    }
+    try {
+      this.licenseSupport = toolProvider.decodeLicense(lic);
+      this.lastLicense = lic;
+      return this.licenseSupport;
+    } catch(NotExceptException e) {
+      log.info("授权失败：" + e.getMessage(), e);
+    }
+    return null;
   }
 
 }
