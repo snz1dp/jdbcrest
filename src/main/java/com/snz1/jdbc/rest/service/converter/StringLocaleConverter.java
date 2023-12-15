d2fc52a9e3dc1470d678398bba09b03015d528f7
痞子飞猪 <13317312768@qq.com>
2023-12-15 22:16:22 +0800

    修改配置

--- src/main/java/com/snz1/jdbc/rest/service/converter/StringLocaleConverter.java
+++ src/main/java/com/snz1/jdbc/rest/service/converter/StringLocaleConverter.java
@@ -1,5 +1,6 @@
 package com.snz1.jdbc.rest.service.converter;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.ParseException;
@@ -26,19 +27,17 @@ public class StringLocaleConverter extends BaseLocaleConverter {
         (value instanceof BigInteger) ||
         (value instanceof Byte) ||
         (value instanceof Short)) {
-
       result = value + "";
     } else if ((value instanceof Double) ||
         (value instanceof BigDecimal) ||
         (value instanceof Float)) {
-
       result = value + "";
     } else if (value instanceof Date) { // java.util.Date, java.sql.Date, java.sql.Time, java.sql.Timestamp
 
       final SimpleDateFormat dateFormat = new SimpleDateFormat(JsonUtils.JsonDateFormat, locale);
 
       result = dateFormat.format(value);
-    } else if (value instanceof Map || value instanceof Date || value instanceof List) {
+    } else if (value instanceof Map || value instanceof Date || value instanceof List || value instanceof Array) {
       result = JsonUtils.toJson(value);
     } else {
       result = value.toString();
